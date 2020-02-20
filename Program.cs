2e77d58865d8d939081664555be89df822cb927a
Michael Hansen <gearsofwarfan97@gmail.com>
2020-02-19 20:54:26 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Iterative_Statement_Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number between 2 and 10 : ");
+
+            int num;
+            try
+            {
+                num = Convert.ToInt32(Console.ReadLine());
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine(" Enter a valid integer.");
+                return;
+            }
+            if (num % 2 == 0)
+            {
+                num++;
+
+                for (int i = 0; i < num; i++)
+                {
+                    Console.WriteLine("You entered an even number");
+                }
+            }
+            else
+            {
+                num--;
+                for(int i = 0; i < num; i ++ )
+                {
+                    Console.WriteLine("You entered an odd number");
+                }
+            }
+            }
+        }
+    }
+
